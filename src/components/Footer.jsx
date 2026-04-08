bc775774e4f1698376532158572ebcea530cc394
Srisivaranjani17 <srisivaranjanim@gmail.com>
2026-04-08 14:13:41 +0530

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
     return (
         <footer className="py-6 md:py-8 bg-secondary/70 border-t border-white/5 text-center">
             <p className="text-text-secondary text-sm">
-                © {new Date().getFullYear()}Designed & Built by Srisivaranjani Developer Portfolio.
+                © {new Date().getFullYear()} Designed & Built by Srisivaranjani Developer Portfolio.
             </p>
         </footer>
     );
